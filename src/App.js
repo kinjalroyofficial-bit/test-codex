8accc3c2723b758061163db608d067221b9f1ba2
kinjalroyofficial-bit <kinjalroy.official@gmail.com>
2026-04-05 20:35:10 +0530

    Update homepage greeting for Kinjal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>HELLO FROM AUTO DEPLOY</h1>
+      <h1>Welcome, Kinjal!</h1>
       <p>Deployment pipeline is working correctly.</p>
     </div>
   );
