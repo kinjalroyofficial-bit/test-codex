1c53ec997a8a8ff04cc7e0d59ad2959d07b54a21
kinjalroyofficial-bit <kinjalroy.official@gmail.com>
2026-04-05 12:53:09 +0000

    Task: Add test notification

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cd /home/react-app
 
@@ -9,6 +10,13 @@ git pull origin main >> deploy.log 2>&1
 npm install >> deploy.log 2>&1
 npm run build >> deploy.log 2>&1
 
-systemctl restart nginx
+systemctl restart nginx >> deploy.log 2>&1
 
 echo "===== DEPLOY END =====" >> deploy.log
+
+BOT_TOKEN="8529655539:AAGOIbipfyUuVmA_nQKOu-EpLFHO7pQv-mY"
+CHAT_ID="8629852323"
+
+curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+-d chat_id=$CHAT_ID \
+-d text="Deployment SUCCESS"
